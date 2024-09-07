91d42688577a5dbd6154ce7b262879a423f5c3db
Egor Egorov <me@egorfine.com>
2024-09-07 13:27:38 +0200

    ts/bun version

--- bot.ts
+++ bot.ts
@@ -1,34 +1,31 @@
-/* eslint-disable camelcase */
 import TelegramBot from 'node-telegram-bot-api';
 
 const bot = new TelegramBot(process.env.TELEGRAM_TOKEN!, { polling: true });
 
 const NOT_WELCOME_MESSAGE = [
-	"Hi. I'm a private bot managing a count of specific Telegram channel.",
-	"There is nothing I can do for you, so goodbye and have a nice day :-)\n\n",
-	"Привет! Я частный бот, работающий только на парочке секретных телеграм каналов,",
-	"поэтому ничем не могу вам быть полезен. До свидания и хорошего дня! :-)\n\n"
+  "Hi. I'm a private bot managing a count of specific Telegram channel.",
+  "There is nothing I can do for you, so goodbye and have a nice day :-)\n\n",
+  "Привет! Я частный бот, работающий только на парочке секретных телеграм каналов,",
+  "поэтому ничем не могу вам быть полезен. До свидания и хорошего дня! :-)\n\n"
 ].join(" ");
 
+function processPrivateMessage(msg: TelegramBot.Message) {
+  const text = (msg.text || '').trim();
 
-function processPrivateMessage(msg) {
-	const text = (msg.text || '').trim();
-	const chatId = String(msg.chat.id);
+  if (text == '/ping') {
+    bot.sendMessage(msg.chat.id, "Pong!");
+    return;
+  }
 
-	if (text == '/ping') {
-		bot.sendMessage(chatId, "Pong!");
-		return;
-	}
-
-  bot.sendMessage(chatId, NOT_WELCOME_MESSAGE, { parse_mode: 'Markdown' });
+  bot.sendMessage(msg.chat.id, NOT_WELCOME_MESSAGE, { parse_mode: 'Markdown' });
 }
 
 /**********************************/
 
 bot.on('message', msg => {
-	const isPrivate = msg.chat?.type != 'supergroup';
+  const isPrivate = msg.chat?.type !== 'supergroup';
 
-	if (!isPrivate) {
+  if (!isPrivate) {
     return;
   }
 
@@ -36,14 +33,14 @@ bot.on('message', msg => {
 });
 
 bot.on('new_chat_members', async msg => {
-	for (const member of msg.new_chat_members!) {
-		if (member.is_bot) {
-			continue;
-		}
+  for (const member of msg.new_chat_members!) {
+    if (member.is_bot) {
+      continue;
+    }
 
-		console.log("Deleted new_chat_members message", msg);
+    console.log("Deleted new_chat_members message", msg);
     bot.deleteMessage(msg.chat.id, msg.message_id);
-	}
+  }
 });
 
 bot.on('polling_error', error => {
